07b302040fd9a5c8d80b19f78f7ce941fa99a916
Vikram Shinde <Shindevikram1210@gmail.com>
2021-03-26 17:34:44 +0530

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,88 @@
+
+ 
+
+var products = JSON.parse(localStorage.getItem("cart"));
+var cartItems = [];
+var cart_n = document.getElementById("cart_n");
+var table = document.getElementById("table");
+var total = 0;
+
+//html
+function tableHTML(i){
+    return `
+            <tr>
+            <th scope="row">${i+1}</th>
+            <td><img style="width:90px;" src="${products[i].url}"></td>
+            <td>${products[i].name}</td>
+            <td>1</td>
+            <td>${products[i].price}</td>
+            </tr>
+
+    `;
+}
+
+function buy(){
+    var d = new Date();
+    var t = d.getTime();
+    var counter = t;
+    counter+=1;
+    const db = firebase.database().ref("order/"+counter);
+    let itemdb  = {
+        id:counter,
+        order:counter-895,
+        total:total
+    }
+    db.set(itemdb);
+    alert("Purchase made successfully");
+    clean();
+}
+
+function clean(){
+    localStorage.clear();
+    for (let index = 0; index < products.length; index++) {
+        table.innerHTML += tableHTML(index);
+        total = total+parseInt(products[index].price);
+        
+    }
+    total = 0;
+    table.innerHTML=`
+    <tr>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    </tr>
+    `;
+    cart_n.innerHTML = '';
+    document.getElementById("btnBuy").style.display="none";
+    document.getElementById("btnClean").style.display="none";
+
+}
+
+function render(){
+    for (let index = 0; index < products.length; index++) {
+        table.innerHTML+=tableHTML(index);
+        total = total+parseInt(products[index].price);
+    }
+    table.innerHTML+=`
+    <tr>
+    <th scope="col"></th>
+    <th scope="col"></th>
+    <th scope="col"></th>
+    <th scope="col"></th>
+    <th scope="col">Total: $${total}.00</th>
+    </tr>
+    <tr>
+    <th scope="col"></th>
+    <th scope="col"></th>
+    <th scope="col"></th>
+    <th scope="col">
+    <button id="btnClean" onclick="clean()" class="btn text-white btn-warning">Clean Shopping Cart</button>
+    </th>
+    <th scope="col"><button id="btnBuy" onclick="buy()" class="btn btn-success">Buy</button></th>
+    </tr>
+    `;
+    products = JSON.parse(localStorage.getItem("cart"));
+    cart_n.innerHTML = `[${products.length}]`;
+}
