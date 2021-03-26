81485630eff81fefb3a7d56383a2f7a87aa3c646
Vikram Shinde <Shindevikram1210@gmail.com>
2021-03-26 17:36:41 +0530

    Add files via upload

---
+++ admin/login.js
@@ -0,0 +1,20 @@
+var x = document.getElementById("email");
+var p = document.getElementById("password");
+document.getElementById("form").addEventListener("submit",(ee)=>{
+    ee.preventDefault();
+    console.log(x.value);
+    console.log(p.value);
+    if(x.value=="admin@gmail.com" && p.value=="123"){
+        alert("Welcome Access Granted");
+        setTimeout(()=>{
+            loadPage();
+        },3000);
+    }
+    else{
+        alert("Error Access denies");
+
+    }
+    function loadPage(){
+        window.location.href="./admin.html";
+    }
+});
